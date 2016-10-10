591559014a962cd657387f2735b256b545af9dab
Istomin, Mikhail <mail@mistler.ru>
2016-10-10 20:18:36 +0700

    common: add method stubs

--- src/convolution.c
+++ src/convolution.c
@@ -1,5 +1,15 @@
 #include "xnor_nn.h"
 
+static xnor_nn_status_t fwd(const void *s,
+        const void *src, const void *weights, void *dst) {
+    const xnor_nn_convolution_t *self = s;
+    (void)self;
+    (void)src;
+    (void)weights;
+    (void)dst;
+    return xnor_nn_success;
+}
+
 xnor_nn_status_t xnor_nn_init_convolution(xnor_nn_convolution_t *c,
         int mb, int oc, int ic, int ih, int iw,
         int kh, int kw, int sh, int sw, int ph, int pw) {
@@ -16,5 +26,6 @@ xnor_nn_status_t xnor_nn_init_convolution(xnor_nn_convolution_t *c,
     (void)ph;
     (void)pw;
 
+    c->forward = fwd;
     return xnor_nn_success;
 }
--- src/weights_binarizer.c
+++ src/weights_binarizer.c
@@ -1,5 +1,19 @@
 #include "xnor_nn.h"
 
+static size_t sz(const void *s){
+    const xnor_nn_weights_binarizer_t *self = s;
+    (void)self;
+    return 1;
+}
+
+static xnor_nn_status_t exec(const void *s, const void *from, void *to) {
+    const xnor_nn_weights_binarizer_t *self = s;
+    (void)self;
+    (void)from;
+    (void)to;
+    return xnor_nn_success;
+}
+
 xnor_nn_status_t xnor_nn_init_weights_binarizer(xnor_nn_weights_binarizer_t *b,
         int oc, int ic, int kh, int kw) {
     (void)b;
@@ -8,5 +22,7 @@ xnor_nn_status_t xnor_nn_init_weights_binarizer(xnor_nn_weights_binarizer_t *b,
     (void)kh;
     (void)kw;
 
+    b->size = sz;
+    b->execute = exec;
     return xnor_nn_success;
 }
