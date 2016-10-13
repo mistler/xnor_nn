1f7fa35eeb270c5d48398e32115025be4923c0e9
Istomin, Mikhail <mail@mistler.ru>
2016-10-13 11:09:51 +0700

    example: introduce c++ convoluiton example

---
+++ example/simple_convolution.cpp
@@ -0,0 +1,34 @@
+#include "xnor_nn.hpp"
+
+int main(){
+    const int MB = 32;
+    const int IC = 64, OC = 128;
+    const int IH = 13, IW = 13;
+    const int OH = 13, OW = 13;
+    const int KH = 3, KW = 3;
+    const int SH = 1, SW = 1;
+    const int PH = 1, PW = 1;
+
+    // Usr data
+    float *src = nullptr, *weights = nullptr;
+    float *dst = nullptr;
+
+    src = new float[MB*IC*IH*IW];
+    weights = new float[OC*IC*KH*KW];
+    dst = new float[MB*OC*OH*OW];
+
+    // Convolution setup
+    xnor_nn::Convolution convolution{MB, OC, IC, IH, IW,
+            KH, KW, SH, SW, PH, PW, weights};
+    delete[] weights;
+
+    // Execution
+    for (int i = 0; i < 3; i++) {
+        convolution.forward(src, dst);
+    }
+
+    delete[] src;
+    delete[] dst;
+
+    return 0;
+}
