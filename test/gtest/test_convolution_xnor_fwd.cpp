75b5238b8aa071ea90ff0f579358511d116a6648
Istomin, Mikhail <mikhail.istomin@intel.com>
2016-10-13 17:54:27 +0700

    test: raw precalculated convolution test (todo: enable)

--- test/gtest/test_convolution_xnor_fwd.cpp
+++ test/gtest/test_convolution_xnor_fwd.cpp
@@ -1,6 +1,69 @@
-#include "xnor_nn.h"
+#include "xnor_nn.hpp"
 #include "gtest.h"
 
 TEST(ConvolutionXnorFwd, simple_precalculated) {
-    EXPECT_EQ(0,0);
+    const int MB = 1;
+    const int IC = 2, OC = 2;
+    const int IH = 3, IW = 3;
+    const int OH = 3, OW = 3;
+    const int KH = 3, KW = 3;
+    const int SH = 1, SW = 1;
+    const int PH = 1, PW = 1;
+
+    const float P = 1.f;
+    const float N = -1.f;
+
+    // Usr data (just random)
+    const float src[MB*IC*IH*IW] = {
+        P, P, P,
+        N, P, N,
+        N, N, N,
+
+        N, N, N,
+        N, N, N,
+        P, P, P
+    };
+    const float weights[OC*IC*KH*KW] = {
+        P, P, P,
+        N, N, P,
+        P, N, N,
+
+        P, P, P,
+        P, P, P,
+        P, P, P,
+
+        N, N, N,
+        N, N, N,
+        N, N, N,
+
+        P, P, P,
+        N, N, P,
+        P, N, N
+    };
+    /*
+    const float expected_dst[MB*OC*OH*OW] = {
+        N // TODO: fill me!
+    };
+    */
+
+    float actual_dst[MB*OC*OH*OW] = { .0f };
+
+    // Convolution setup
+    xnor_nn::Convolution convolution{MB, OC, IC, IH, IW,
+            KH, KW, SH, SW, PH, PW, weights};
+
+    // Execution
+    convolution.forward(src, actual_dst);
+
+    // Check result
+    /*
+    for (int mb = 0; mb < MB; mb++)
+    for (int oc = 0; oc < OC; oc++)
+    for (int oh = 0; oh < OH; oh++)
+    for (int ow = 0; ow < OW; ow++) {
+        float actual = actual_dst[((mb*OC + oc)*OH + oh)*OW + ow];
+        float expected = expected_dst[((mb*OC + oc)*OH + oh)*OW + ow];
+        EXPECT_FLOAT_EQ(expected, actual);
+    }
+    */
 }
