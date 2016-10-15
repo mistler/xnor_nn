e4989217cb3c8d72416253aa8ec02164f000a2f8
Istomin, Mikhail <mail@mistler.ru>
2016-10-15 12:13:42 +0700

    verbose: add getenv

--- src/utils/logger.hpp
+++ src/utils/logger.hpp
@@ -3,23 +3,28 @@
 
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 namespace xnor_nn {
 namespace utils {
 
 class Logger {
 public:
-    Logger(): info_stream(std::cout) {}
+    Logger(): info_stream_(std::cout), is_verbose_(false) {
+        read_environment();
+    }
 
     template<typename ... T>
     static void info(T ... t) {
         Logger &inst = instance();
-        inst.info_stream << "xnor_nn: info:";
+        if (!inst.is_verbose_) return;
+        inst.info_stream_ << "xnor_nn: info:";
         inst.info_variadic(t...);
     }
 
 private:
-    std::ostream &info_stream;
+    std::ostream &info_stream_;
+    bool is_verbose_;
 
 private:
     static Logger &instance() {
@@ -31,16 +36,21 @@ private:
     Logger& operator=(Logger const&) = delete;
     Logger& operator=(Logger &&) = delete;
 
+    void read_environment() {
+        char *env = getenv("XNOR_NN_VERBOSE");
+        if (env != NULL && env[0] == '1') is_verbose_ = true;
+    }
+
     // Variadic template magic
     void info_variadic() {
         Logger &inst = instance();
-        inst.info_stream << std::endl;
+        inst.info_stream_ << std::endl;
     }
 
     template<typename T0, typename ... T>
     void info_variadic(const T0& t0, T ... t) {
         Logger &inst = instance();
-        inst.info_stream << " " << t0;
+        inst.info_stream_ << " " << t0;
         inst.info_variadic(t...);
     }
 };
