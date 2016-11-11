c72bf2adb8b8c633579ee3519b7fbbbbaa7b0d7d
Istomin, Mikhail <mikhail.istomin@intel.com>
2016-11-11 18:20:06 +0700

    test: gtest: convolution: introduce reference test

---
+++ test/gtest/test_convolution_compare_with_reference.cpp
@@ -0,0 +1,79 @@
+#include "xnor_nn.hpp"
+
+#include "gtest.h"
+#include "common.hpp"
+
+typedef struct {
+    int mb;
+    int ic, oc;
+    int ih, iw;
+    int oh, ow;
+    int kh, kw;
+    int sh, sw;
+    int ph, pw;
+    xnor_nn_algorithm_t algorithm;
+} params_t;
+
+static void fill_src(float *d, const params_t &p) {
+#   pragma omp parallel for collapse(3) schedule(static)
+    for (int mb = 0; mb < p.mb; mb++)
+    for (int ic = 0; ic < p.ic; ic++)
+    for (int ih = 0; ih < p.ih; ih++)
+    for (int iw = 0; iw < p.iw; iw++)
+        d[((mb*p.ic + ic)*p.ih + ih)*p.iw + iw] =
+            0.33f + 3.14f*(19 - (iw+ih+ic+mb%37));
+}
+static void fill_weights(float *d, const params_t &p) {
+#   pragma omp parallel for collapse(3) schedule(static)
+    for (int oc = 0; oc < p.oc; oc++)
+    for (int ic = 0; ic < p.ic; ic++)
+    for (int kh = 0; kh < p.kh; kh++)
+    for (int kw = 0; kw < p.kw; kw++)
+        d[((oc*p.ic + ic)*p.kh + kh)*p.kw + kw] =
+            0.18f + 1.12f*(11 - (kw+kh+ic+oc%23));
+}
+
+class ConvolutionForwardOptimized : public ::testing::TestWithParam<params_t> {
+protected:
+    virtual void SetUp() {
+        params_t p = ::testing::TestWithParam<params_t>::GetParam();
+
+        float *src = new float[p.mb*p.ic*p.ih*p.iw];
+        float *weights = new float[p.oc*p.ic*p.kh*p.kw];
+        float *dst = new float[p.mb*p.oc*p.oh*p.ow];
+        float *ref_dst = new float[p.mb*p.oc*p.oh*p.ow];
+
+        fill_src(src, p);
+        fill_weights(weights, p);
+
+        // Optimized
+        xnor_nn::Convolution convolution{p.algorithm,
+                p.mb, p.oc, p.ic, p.ih, p.iw, p.kh, p.kw,
+                p.sh, p.sw, p.ph, p.pw, weights};
+
+        convolution.forward(src, dst);
+
+        // Reference
+        xnor_nn::Convolution reference_convolution{xnor_nn_algorithm_reference,
+                p.mb, p.oc, p.ic, p.ih, p.iw, p.kh, p.kw,
+                p.sh, p.sw, p.ph, p.pw, weights};
+
+        reference_convolution.forward(src, ref_dst);
+
+        xnor_nn::test::check_4d(p.mb, p.oc, p.oh, p.ow, dst, ref_dst);
+
+        delete[] src;
+        delete[] weights;
+        delete[] dst;
+        delete[] ref_dst;
+    }
+};
+
+TEST_P(ConvolutionForwardOptimized, compare_with_reference)
+{
+}
+
+INSTANTIATE_TEST_CASE_P(TestConvolutionForward,
+        ConvolutionForwardOptimized, ::testing::Values(
+params_t{ 3, 3, 64, 224, 224, 11, 11, 4, 4, 2, 2, xnor_nn_algorithm_optimized },
+params_t{ 3, 64, 192, 27, 27, 5, 5, 1, 1, 2, 2, xnor_nn_algorithm_optimized }));
