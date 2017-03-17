230c93c102e7e9be0f957a6f8632cca28c09bd1e
Istomin, Mikhail <mail@mistler.ru>
2017-03-18 00:22:22 +0700

    x86: use unroll ow, popcnt

--- src/x86/bcast_convolution_avx.cpp
+++ src/x86/bcast_convolution_avx.cpp
@@ -15,57 +15,186 @@ inline static void execute(const int MB, const int OC,
         const int KH, const int KW, const int SH, const int SW,
         const int PH, const int PW,
         const int ICO, const int OCO, const int OCI, const int VLEN,
-        const int *src, const int *weights, const float *alpha, const float *k,
-        float *dst) {
+        const float alpha,
+        const int *src, const int *weights, const float *k, float *dst) {
     (void)VLEN;
-    const int ones[8] = {-1,-1,-1,-1,-1,-1,-1,-1};
 
-    // TODO: potentially loops can be reordered
-    // TODO: check collapse value for performance
-#   pragma omp parallel for collapse(4) schedule(static)
+    constexpr int MAX_ICO_UNROLL = 12;
+    constexpr int MAX_OW_UNROLL = 4;
+
+    int unroll_ow = 1;
+    for (int uow = 0; uow < MAX_OW_UNROLL; uow++)
+        if (OW % uow == 0) unroll_ow = uow;
+
+#   pragma omp parallel for collapse(3) schedule(static)
     for (int mb = 0; mb < MB; mb++)
     for (int oco = 0; oco < OCO; oco++)
     for (int oh = 0; oh < OH; oh++)
-    for (int ow = 0; ow < OW; ow++) {
-        int operations_counter = 0;
-        int d_arr[16] = { 0 };
-        __m256 v_ones = _mm256_loadu_ps((float*)ones);
+    for (int ow = 0; ow < OW; ow += unroll_ow) {
+        int operations_counter[MAX_OW_UNROLL] = {0};
+
+        __m128i d_arr[MAX_OW_UNROLL][2];
+        for (int i = 0; i < MAX_OW_UNROLL; i++)
+        for (int k = 0; k < 2; k++) d_arr[i][k] = _mm_set1_epi8(0);
+
+        __m256 v_ones = _mm256_castsi256_ps(_mm256_set1_epi32(-1));
 
         for (int kh = 0; kh < KH; kh++)
         for (int kw = 0; kw < KW; kw++) {
-            const int ih = oh*SH - PH + kh;
-            const int iw = ow*SW - PW + kw;
+            const int *weights_ic_oci = weights + ((oco*KH +kh)*KW + kw)*ICO*OCI;
 
-            if (ih < 0 || iw < 0) continue;
-            if (ih >= IH || iw >= IW) continue;
+            __m256 v_weights[MAX_ICO_UNROLL];
+            for (int ico = 0; ico < ICO; ico++)
+                v_weights[ico] = _mm256_load_ps((float*)weights_ic_oci + ico*OCI);
 
-            const int *src_ic = src + ((mb*IH + ih)*IW + iw)*ICO;
-            const int *weights_ic_oci =
-                weights + ((oco*KH +kh)*KW + kw)*ICO*OCI;
+            for (int uow = 0; uow < unroll_ow; uow++) {
+                const int ih = oh*SH - PH + kh;
+                const int iw = (ow + uow)*SW - PW + kw;
 
-            operations_counter += IC;
-            for (int ico = 0; ico < ICO; ico++) {
-                __m256 v_src = _mm256_broadcast_ss((float*)src_ic + ico);
-                __m256 v_weights = _mm256_load_ps(
-                        (float*)weights_ic_oci + ico*OCI);
+                if (PH != 0 || PW != 0) { // May be incorrect for non symmetr p
+                    if (ih < 0 || iw < 0) continue;
+                    if (ih >= IH || iw >= IW) continue;
+                }
+                operations_counter[uow] += IC;
 
-                __m256 v_xor = _mm256_xor_ps(v_src, v_weights);
-                __m256i v_xnor =
-                    _mm256_castps_si256(_mm256_xor_ps(v_xor, v_ones));
+                const int *src_ic = src + ((mb*IH + ih)*IW + iw)*ICO;
 
-                d_arr[0] += __builtin_popcount(_mm256_extract_epi32(v_xnor, 0));
-                d_arr[1] += __builtin_popcount(_mm256_extract_epi32(v_xnor, 1));
-                d_arr[2] += __builtin_popcount(_mm256_extract_epi32(v_xnor, 2));
-                d_arr[3] += __builtin_popcount(_mm256_extract_epi32(v_xnor, 3));
-                d_arr[4] += __builtin_popcount(_mm256_extract_epi32(v_xnor, 4));
-                d_arr[5] += __builtin_popcount(_mm256_extract_epi32(v_xnor, 5));
-                d_arr[6] += __builtin_popcount(_mm256_extract_epi32(v_xnor, 6));
-                d_arr[7] += __builtin_popcount(_mm256_extract_epi32(v_xnor, 7));
+                for (int ico = 0; ico < ICO; ico++) {
+                    __m256 v_src = _mm256_broadcast_ss((float*)src_ic + ico);
+
+                    const __m256 v_xor = _mm256_xor_ps(v_src, v_weights[ico]);
+                    const __m256i v_xnor = _mm256_castps_si256(_mm256_xor_ps(v_xor, v_ones));
+
+                    /*
+                    int tmp[8] __attribute__ ((aligned(64)));
+                    tmp[0] = __builtin_popcount(_mm256_extract_epi32(v_xnor, 0));
+                    tmp[1] = __builtin_popcount(_mm256_extract_epi32(v_xnor, 1));
+                    tmp[2] = __builtin_popcount(_mm256_extract_epi32(v_xnor, 2));
+                    tmp[3] = __builtin_popcount(_mm256_extract_epi32(v_xnor, 3));
+                    tmp[4] = __builtin_popcount(_mm256_extract_epi32(v_xnor, 4));
+                    tmp[5] = __builtin_popcount(_mm256_extract_epi32(v_xnor, 5));
+                    tmp[6] = __builtin_popcount(_mm256_extract_epi32(v_xnor, 6));
+                    tmp[7] = __builtin_popcount(_mm256_extract_epi32(v_xnor, 7));
+                    d_arr[uow][0] = _mm_add_epi32(d_arr[uow][0], _mm_load_si128((const __m128i*)(tmp + 0)));
+                    d_arr[uow][1] = _mm_add_epi32(d_arr[uow][1], _mm_load_si128((const __m128i*)(tmp + 4)));
+                    */
+
+                    // 1x6 + 2x4
+                    /*
+                    const __m128i v_mask_4 = _mm_set1_epi8(0x0F);
+                    const __m128i v_mask_8 = _mm_set1_epi16(0x00FF);
+                    const __m128i v_mask_16 = _mm_set1_epi32(0x0000FFFF);
+                    const __m128i v_popcnt_table = _mm_setr_epi8(0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4);
+
+                    const __m128i lo_v_xnor = _mm256_extractf128_si256(v_xnor, 0);
+                    const __m128i lo_v_popcnt_8hi = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(lo_v_xnor, v_mask_4));
+                    const __m128i lo_v_popcnt_8lo = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(_mm_srli_epi16(lo_v_xnor, 4), v_mask_4));
+                    const __m128i lo_v_popcnt_8 = _mm_add_epi8(lo_v_popcnt_8lo, lo_v_popcnt_8hi);
+                    const __m128i lo_v_popcnt_16hi = _mm_and_si128(lo_v_popcnt_8, v_mask_8);
+                    const __m128i lo_v_popcnt_16lo = _mm_and_si128(_mm_srli_epi16(lo_v_popcnt_8, 8), v_mask_8);
+                    const __m128i lo_v_popcnt_16 = _mm_add_epi16(lo_v_popcnt_16lo, lo_v_popcnt_16hi);
+                    const __m128i lo_v_popcnt_32hi = _mm_and_si128(lo_v_popcnt_16, v_mask_16);
+                    const __m128i lo_v_popcnt_32lo = _mm_and_si128(_mm_srli_epi32(lo_v_popcnt_16, 16), v_mask_16);
+                    d_arr[uow][0] = _mm_add_epi32(d_arr[uow][0], lo_v_popcnt_32lo);
+                    d_arr[uow][0] = _mm_add_epi32(d_arr[uow][0], lo_v_popcnt_32hi);
+
+                    const __m128i hi_v_xnor = _mm256_extractf128_si256(v_xnor, 1);
+                    const __m128i hi_v_popcnt_8lo = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(hi_v_xnor, v_mask_4));
+                    const __m128i hi_v_popcnt_8hi = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(_mm_srli_epi16(hi_v_xnor, 4), v_mask_4));
+                    const __m128i hi_v_popcnt_8 = _mm_add_epi32(hi_v_popcnt_8hi, hi_v_popcnt_8lo);
+                    const __m128i hi_v_popcnt_16hi = _mm_and_si128(hi_v_popcnt_8, v_mask_8);
+                    const __m128i hi_v_popcnt_16lo = _mm_and_si128(_mm_srli_epi16(hi_v_popcnt_8, 8), v_mask_8);
+                    const __m128i hi_v_popcnt_16 = _mm_add_epi16(hi_v_popcnt_16lo, hi_v_popcnt_16hi);
+                    const __m128i hi_v_popcnt_32hi = _mm_and_si128(hi_v_popcnt_16, v_mask_16);
+                    const __m128i hi_v_popcnt_32lo = _mm_and_si128(_mm_srli_epi32(hi_v_popcnt_16, 16), v_mask_16);
+                    d_arr[uow][1] = _mm_add_epi32(d_arr[uow][1], hi_v_popcnt_32lo);
+                    d_arr[uow][1] = _mm_add_epi32(d_arr[uow][1], hi_v_popcnt_32hi);
+                    */
+
+                    // 2x6 + 1x4
+                    /*
+                    //const __m128i v_mask_4 = _mm_set1_epi8(0x0F);
+                    const __m128i v_mask_8 = _mm_set1_epi16(0x000F);
+                    const __m128i v_mask_16 = _mm_set1_epi32(0x0000FFFF);
+                    const __m128i v_popcnt_table = _mm_setr_epi8(0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4);
+
+                    const __m128i lo_v_xnor = _mm256_extractf128_si256(v_xnor, 0);
+                    const __m128i lo_v_popcnt_8hi = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(lo_v_xnor, v_mask_8));
+                    const __m128i lo_v_popcnt_8lo = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(_mm_srli_epi16(lo_v_xnor, 4), v_mask_8));
+                    const __m128i lo_v_popcnt_8 = _mm_add_epi16(lo_v_popcnt_8lo, lo_v_popcnt_8hi);
+                    const __m128i lo_v_popcnt_16hi = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(_mm_srli_epi16(lo_v_xnor, 8), v_mask_8));
+                    const __m128i lo_v_popcnt_16lo = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(_mm_srli_epi16(lo_v_xnor, 12), v_mask_8));
+                    const __m128i lo_v_popcnt_16 = _mm_add_epi16(_mm_add_epi16(lo_v_popcnt_16lo, lo_v_popcnt_16hi), lo_v_popcnt_8);
+                    const __m128i lo_v_popcnt_32hi = _mm_and_si128(lo_v_popcnt_16, v_mask_16);
+                    const __m128i lo_v_popcnt_32lo = _mm_and_si128(_mm_srli_epi32(lo_v_popcnt_16, 16), v_mask_16);
+                    d_arr[uow][0] = _mm_add_epi32(d_arr[uow][0], lo_v_popcnt_32lo);
+                    d_arr[uow][0] = _mm_add_epi32(d_arr[uow][0], lo_v_popcnt_32hi);
+
+                    const __m128i hi_v_xnor = _mm256_extractf128_si256(v_xnor, 1);
+                    const __m128i hi_v_popcnt_8lo = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(hi_v_xnor, v_mask_8));
+                    const __m128i hi_v_popcnt_8hi = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(_mm_srli_epi16(hi_v_xnor, 4), v_mask_8));
+                    const __m128i hi_v_popcnt_8 = _mm_add_epi16(hi_v_popcnt_8hi, hi_v_popcnt_8lo);
+                    const __m128i hi_v_popcnt_16hi = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(_mm_srli_epi16(hi_v_xnor, 8), v_mask_8));
+                    const __m128i hi_v_popcnt_16lo = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(_mm_srli_epi16(hi_v_xnor, 12), v_mask_8));
+                    const __m128i hi_v_popcnt_16 = _mm_add_epi16(_mm_add_epi16(hi_v_popcnt_16lo, hi_v_popcnt_16hi), hi_v_popcnt_8);
+                    const __m128i hi_v_popcnt_32hi = _mm_and_si128(hi_v_popcnt_16, v_mask_16);
+                    const __m128i hi_v_popcnt_32lo = _mm_and_si128(_mm_srli_epi32(hi_v_popcnt_16, 16), v_mask_16);
+                    d_arr[uow][1] = _mm_add_epi32(d_arr[uow][1], hi_v_popcnt_32lo);
+                    d_arr[uow][1] = _mm_add_epi32(d_arr[uow][1], hi_v_popcnt_32hi);
+                    */
+
+                    // mix
+                    const __m128i v_mask_4 = _mm_set1_epi8(0x0F);
+                    const __m128i v_mask_8 = _mm_set1_epi16(0x000F);
+                    const __m128i v_mask_8hi = _mm_set1_epi16(0x00FF);
+                    const __m128i v_mask_16 = _mm_set1_epi32(0x0000FFFF);
+                    const __m128i v_popcnt_table = _mm_setr_epi8(0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4);
+
+                    const __m128i lo_v_xnor = _mm256_extractf128_si256(v_xnor, 0);
+                    const __m128i lo_v_popcnt_8hi = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(lo_v_xnor, v_mask_8));
+                    const __m128i lo_v_popcnt_8lo = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(_mm_srli_epi16(lo_v_xnor, 4), v_mask_8));
+                    const __m128i lo_v_popcnt_8 = _mm_add_epi16(lo_v_popcnt_8lo, lo_v_popcnt_8hi);
+                    const __m128i lo_v_popcnt_16hi = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(_mm_srli_epi16(lo_v_xnor, 8), v_mask_8));
+                    const __m128i lo_v_popcnt_16lo = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(_mm_srli_epi16(lo_v_xnor, 12), v_mask_8));
+                    const __m128i lo_v_popcnt_16 = _mm_add_epi16(_mm_add_epi16(lo_v_popcnt_16lo, lo_v_popcnt_16hi), lo_v_popcnt_8);
+                    const __m128i lo_v_popcnt_32hi = _mm_and_si128(lo_v_popcnt_16, v_mask_16);
+                    const __m128i lo_v_popcnt_32lo = _mm_and_si128(_mm_srli_epi32(lo_v_popcnt_16, 16), v_mask_16);
+                    d_arr[uow][0] = _mm_add_epi32(d_arr[uow][0], lo_v_popcnt_32lo);
+                    d_arr[uow][0] = _mm_add_epi32(d_arr[uow][0], lo_v_popcnt_32hi);
+
+                    const __m128i hi_v_xnor = _mm256_extractf128_si256(v_xnor, 1);
+                    const __m128i hi_v_popcnt_8lo = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(hi_v_xnor, v_mask_4));
+                    const __m128i hi_v_popcnt_8hi = _mm_shuffle_epi8(v_popcnt_table, _mm_and_si128(_mm_srli_epi16(hi_v_xnor, 4), v_mask_4));
+                    const __m128i hi_v_popcnt_8 = _mm_add_epi32(hi_v_popcnt_8hi, hi_v_popcnt_8lo);
+                    const __m128i hi_v_popcnt_16hi = _mm_and_si128(hi_v_popcnt_8, v_mask_8hi);
+                    const __m128i hi_v_popcnt_16lo = _mm_and_si128(_mm_srli_epi16(hi_v_popcnt_8, 8), v_mask_8hi);
+                    const __m128i hi_v_popcnt_16 = _mm_add_epi16(hi_v_popcnt_16lo, hi_v_popcnt_16hi);
+                    const __m128i hi_v_popcnt_32hi = _mm_and_si128(hi_v_popcnt_16, v_mask_16);
+                    const __m128i hi_v_popcnt_32lo = _mm_and_si128(_mm_srli_epi32(hi_v_popcnt_16, 16), v_mask_16);
+                    d_arr[uow][1] = _mm_add_epi32(d_arr[uow][1], hi_v_popcnt_32lo);
+                    d_arr[uow][1] = _mm_add_epi32(d_arr[uow][1], hi_v_popcnt_32hi);
+                }
             }
         }
-        for (int i = 0; i < OCI; i++)
-            dst[((mb*OC + oco*OCI + i)*OH + oh)*OW + ow] =
-                (d_arr[i]*2 - operations_counter) * *alpha * k[oh*OW + ow];
+
+        for (int uow = 0; uow < unroll_ow; uow++) {
+            dst[((mb*OC + oco*OCI + 0)*OH + oh)*OW + (ow+uow)] =
+                (_mm_extract_epi32(d_arr[uow][0], 0)*2 - operations_counter[uow]) * alpha * k[oh*OW + (ow+uow)];
+            dst[((mb*OC + oco*OCI + 1)*OH + oh)*OW + (ow+uow)] =
+                (_mm_extract_epi32(d_arr[uow][0], 1)*2 - operations_counter[uow]) * alpha * k[oh*OW + (ow+uow)];
+            dst[((mb*OC + oco*OCI + 2)*OH + oh)*OW + (ow+uow)] =
+                (_mm_extract_epi32(d_arr[uow][0], 2)*2 - operations_counter[uow]) * alpha * k[oh*OW + (ow+uow)];
+            dst[((mb*OC + oco*OCI + 3)*OH + oh)*OW + (ow+uow)] =
+                (_mm_extract_epi32(d_arr[uow][0], 3)*2 - operations_counter[uow]) * alpha * k[oh*OW + (ow+uow)];
+            dst[((mb*OC + oco*OCI + 4)*OH + oh)*OW + (ow+uow)] =
+                (_mm_extract_epi32(d_arr[uow][1], 0)*2 - operations_counter[uow]) * alpha * k[oh*OW + (ow+uow)];
+            dst[((mb*OC + oco*OCI + 5)*OH + oh)*OW + (ow+uow)] =
+                (_mm_extract_epi32(d_arr[uow][1], 1)*2 - operations_counter[uow]) * alpha * k[oh*OW + (ow+uow)];
+            dst[((mb*OC + oco*OCI + 6)*OH + oh)*OW + (ow+uow)] =
+                (_mm_extract_epi32(d_arr[uow][1], 2)*2 - operations_counter[uow]) * alpha * k[oh*OW + (ow+uow)];
+            dst[((mb*OC + oco*OCI + 7)*OH + oh)*OW + (ow+uow)] =
+                (_mm_extract_epi32(d_arr[uow][1], 3)*2 - operations_counter[uow]) * alpha * k[oh*OW + (ow+uow)];
+        }
     }
 }
 
@@ -105,7 +234,7 @@ xnor_nn_status_t BcastConvolution::exec(
             "Algorithm:", "bcast", "ISA:", "AVX", "Templated");
 
     execute(MB, OC, OH, OW, IC, IH, IW, KH, KW, SH, SW, PH, PW,
-        ICO, OCO, OCI, VLEN, src, weights, alpha, k, dst);
+        ICO, OCO, OCI, VLEN, *alpha, src, weights, k, dst);
 
     return xnor_nn_success;
 }
@@ -160,7 +289,7 @@ xnor_nn_status_t BcastConvolution::exec(
             "Algorithm:", "bcast", "ISA:", "AVX");
 
     execute(MB, OC, OH, OW, IC, IH, IW, KH, KW, SH, SW, PH, PW,
-        ICO, OCO, OCI, VLEN, src, weights, alpha, k, dst);
+        ICO, OCO, OCI, VLEN, *alpha, src, weights, k, dst);
 
     return xnor_nn_success;
 }
